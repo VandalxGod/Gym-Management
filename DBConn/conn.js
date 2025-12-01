1dd4806a47e91c512b25a24d20ceb0d7b68ae0cc
VandalxGod <sumit.tutu2506@gmail.com>
2025-12-01 16:18:04 +0530

    Adding Backend

--- DBConn/conn.js
+++ DBConn/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/gymBackend')
+mongoose.connect('mongodb+srv://sumittutu2506_db_user:ddvbbm6po5CEdUV3@gym.vhgxlrb.mongodb.net/')
   .then(() => console.log('DB connection successful!'))
   .catch(err => {
     console.log(err)
